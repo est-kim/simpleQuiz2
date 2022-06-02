09fb3d04a5dbf15c745dd269b4e42b04193ce118
Esther Kim <kim.esther117@gmail.com>
2022-06-02 00:27:27 -0700

    Created JS game functions and added questions

--- app.js
+++ app.js
@@ -1,140 +1,134 @@
-// const quizContainer = document.getElementById('quiz')
-// const resultsContainer = document.getElementById('results')
-// const  submitButton = document.getElementById('submit')
+const question = document.getElementById('question')
+const choices = Array.from(document.getElementsByClassName('choice-text'))
+//makes it into an array - can test out by console log
 
-// function buildQuiz() {
-//     const output = [] // empty variable to store the HTML output
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
 
-//     //for each question... 
-//     myQuestions.forEach((currentQuestion, questionNumber) => {
-//         const answers = [] //empty variable to store possible list of answers
+let questions = [
+    {
+        question: 'In which country do red onions originate?',
+        choice1: 'China',
+        choice2: 'South Africa',
+        choice3: 'Italy',
+        choice4: 'France',
+        answer: 3
+    },
+    {
+        question: 'What is the most popular soup flavor sold in the UK?',
+        choice1: 'Tomato',
+        choice2: 'Split Pea and Barley',
+        choice3: 'Potato and Leek',
+        choice4: 'Chicken and Mushroom',
+        answer: 1
+    },
+    {
+        question: 'What type of pastry are profiteroles made out of?',
+        choice1: 'Phyllo Pastry',
+        choice2: 'Choux Pastry',
+        choice3: 'Croissant Dough',
+        choice4: 'Flaky Pastry',
+        answer: 2
+    },
+    {
+        question: 'Grenadine is obtained from which fruit?',
+        choice1: 'Maraschino Cherry',
+        choice2: 'Meyer Lemon',
+        choice3: 'Black Cherry',
+        choice4: 'Pomegranate',
+        answer: 4
+    },
+    {
+        question: 'How many kernels will you find on an average ear of corn?',
+        choice1: '200',
+        choice2: '400',
+        choice3: '600',
+        choice4: '800',
+        answer: 4
+    },
+    {
+        question: 'How many segments are in most oranges?',
+        choice1: '6',
+        choice2: '8',
+        choice3: '10',
+        choice4: '12',
+        answer: 3
+    },
+    {
+        question: 'Which is the most stolen food in the world?',
+        choice1: 'Beef ribs',
+        choice2: 'Bread',
+        choice3: 'Cheese',
+        choice4: 'Rice',
+        answer: 3
+    },
+    {
+        question: 'What is the most widely eaten meat in the world?',
+        choice1: 'Pork',
+        choice2: 'Beef',
+        choice3: 'Chicken',
+        choice4: 'Fish',
+        answer: 1
+    },
+    {
+        question: 'In which country was ice cream invented?',
+        choice1: 'Mexico',
+        choice2: 'China',
+        choice3: 'Italy',
+        choice4: 'Germany',
+        answer: 2
+    },
+    {
+        question: 'What was the first food eaten in space?',
+        choice1: 'Chocolate Bar',
+        choice2: 'Peanuts',
+        choice3: 'Beef Jerky',
+        choice4: 'Applesauce',
+        answer: 4
+    },
+]
 
-//         //for each available answer... 
-//         for(letter in currentQuestion.answers) {
-//             //add an HTML radio button
-//             answers.push(
-//                 `<label>
-//                 <input type="radio" name="question${questionNumber}" value="${letter}">
-//                 ${letter} :
-//                 ${currentQuestion.answers[letter]}
-//               </label>`
-//             )
-//         }
-//         output.push(
-//             `<div class = "question"> ${currentQuestion.question} </div>
-//             <div class ="answers"> ${answers.join('')} </div>`
-//         )
+//CONSTANTS 
+const CORRECT_BONUS = 10
+const MAX_QUESTIONS = 10
 
-//     })
-//     quizContainer.innerHTML = output.join('')
-// }
-// function showResults(){
-// }
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions] //need full copy to spread questions into new array
+    getNewQuestion();
+}
 
-// buildQuiz();
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        //go to the end page 
+        return window.location.assign('/end.html')
+    }
+    questionCounter++
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionIndex]
+    question.innerText = currentQuestion.question
 
-// submitButton.addEventListener('click', showResults)
+    choices.forEach( choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+    //takes availableQuestions array and get rid of the question we just used so it doesn't repeat
+    availableQuestions.splice(questionIndex, 1) 
+    acceptingAnswers = true
+}
 
-// const myQuestions = [
-//     {
-//       question: "In which country do red onions originate?",
-//       answers: {
-//         a: "China",
-//         b: "South Africa",
-//         c: "Italy",
-//         d: "France"
-//       },
-//       correctAnswer: "c"
-//     },
-//     {
-//       question: "What is the most popular soup flavor sold in the UK?",
-//       answers: {
-//         a: "Tomato",
-//         b: "Split pea and barley",
-//         c: "Potato and Leek",
-//         d: "Chicken and Mushroom"
-//       },
-//       correctAnswer: "a"
-//     },
-//     {
-//       question: "What type of pastry are profiteroles made out of?",
-//       answers: {
-//         a: "Phyllo Pastry",
-//         b: "Choux Pastry",
-//         c: "Croissant Dough",
-//         d: "Flaky Pastry"
-//       },
-//       correctAnswer: "b"
-//     },
-//     {
-//       question: "Grenadine is obtained from which fruit?",
-//       answers: {
-//         a: "Maraschino Cherry",
-//         b: "Meyer Lemon",
-//         c: "Black Cherry",
-//         d: "Pomegranate"
-//       },
-//       correctAnswer: "d"
-//     },
-//     {
-//       question: "How many kernels will you find on an average ear of corn?",
-//       answers: {
-//         a: "200",
-//         b: "400",
-//         c: "600",
-//         d: "800"
-//       },
-//       correctAnswer: "d"
-//     },
-//     {
-//       question: "How many segments are in most oranges?",
-//       answers: {
-//         a: "6",
-//         b: "8",
-//         c: "10",
-//         d: "12"
-//       },
-//       correctAnswer: "c"
-//     },
-//     {
-//       question: "Which is the most stolen food in the world?",
-//       answers: {
-//         a: "Beef ribs",
-//         b: "Bread",
-//         c: "Cheese",
-//         d: "Rice"
-//       },
-//       correctAnswer: "c"
-//     },
-//     {
-//       question: "What is the most wifely eaten meat in the world?",
-//       answers: {
-//         a: "Pork",
-//         b: "Beef",
-//         c: "Chicken",
-//         d: "Fish"
-//       },
-//       correctAnswer: "a"
-//     },
-//     {
-//       question: "In which country was ice cream invented?",
-//       answers: {
-//         a: "Mexico",
-//         b: "China",
-//         c: "Italy",
-//         d: "Germany"
-//       },
-//       correctAnswer: "b"
-//     },
-//     {
-//       question: "What was the first food eaten in space?",
-//       answers: {
-//         a: "Chocolate Bar",
-//         b: "Peanuts",
-//         c: "Beef Jerky",
-//         d: "Applesauce"
-//       },
-//       correctAnswer: "d"
-//     }
-//   ];
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+        getNewQuestion()
+    })
+})
 
+startGame()
